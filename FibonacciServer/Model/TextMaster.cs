501a43f02c996e455123492f7d5571985364fc35
Palkin Danil <danyapalkin@gmail.com>
2022-03-16 01:04:44 +0200

    TakeNumbersFromText method reworked

--- FibonacciServer/Model/TextMaster.cs
+++ FibonacciServer/Model/TextMaster.cs
@@ -8,39 +8,80 @@ namespace FibonacciServer
 {
     public static class TextMaster
     {
-        public static RowOfNumbers [] TakeNumbersFromText(string text)
+        //public static RowOfNumbers[] TakeNumbersFromText(string text)
+        //{
+        //    string regex = "[a-zA-Zа-яА-Я]+|[a-zA-Zа-яА-Я]";
+        //    const string specialSymbol = "?";
+        //    List<RowOfNumbers> rowsOFNumbers = new List<RowOfNumbers>();
+        //    foreach (string row in text.Split(specialSymbol))
+        //    {
+        //        if (Regex.IsMatch(row, regex))
+        //        {
+        //            string[] symbols = row.Split(',');
+        //            RowOfNumbers numbers = new RowOfNumbers();
+        //            numbers.Numbers = new int[symbols.Length];
+        //            for (int i = 0; i < symbols.Length; i++)
+        //            {
+        //                numbers.Numbers[i] = Int32.Parse(symbols[i]);
+        //            }
+        //            rowsOFNumbers.Add(numbers);
+        //        }
+        //    }
+        //    if (rowsOFNumbers.Count <= 0)
+        //    {
+        //        throw new Exception("No number rows");
+        //    }
+        //    return rowsOFNumbers.ToArray();
+        //}
+        public static RowOfNumbers[] TakeNumbersFromText(string text)
         {
-            string regex = "[a-zA-Zа-яА-Я]+|[a-zA-Zа-яА-Я]";
+            const string specialSymbol = "?";
+            const string Space = " ";
+            const string Comma = ",";
+            const string CommaWithSpace = ", ";
             List<RowOfNumbers> rowsOFNumbers = new List<RowOfNumbers>();
-            foreach (string row in text.Split(" "))
+            foreach (string row in text.Split(specialSymbol))
             {
-                if(!Regex.IsMatch(row, regex))
-                { 
-                    string[] symbols = row.Split(',');
-                    RowOfNumbers numbers = new RowOfNumbers();
-                    numbers.Numbers = new int[symbols.Length];
-                   for(int i = 0; i < symbols.Length; i++)
-                    {
-                        numbers.Numbers[i] = Int32.Parse(symbols[i]);
-                    }
-                    rowsOFNumbers.Add(numbers);
+                if (isMatch(Space, row))
+                {
+                   rowsOFNumbers.Add(obtainNumbersFromRow(Space, row));
+                }
+                if (isMatch(Comma, row))
+                {
+                    rowsOFNumbers.Add(obtainNumbersFromRow(Comma, row));
+                }
+                if (isMatch(CommaWithSpace, row))
+                {
+                    rowsOFNumbers.Add(obtainNumbersFromRow(CommaWithSpace, row));
                 }
             }
             if (rowsOFNumbers.Count <= 0)
             {
-                throw new Exception("No number rows");
+                throw new Exception("No consistent rows");
             }
             return rowsOFNumbers.ToArray();
         }
-        //public static string MatchTest( string text)
-        //{
-        //    return Regex.Matches(text, "(\d,)*\d{1}");
-        //}
+        private static bool isMatch(string separator,string row)
+        {
+            string regex = @"(\d,)*\d{1}";
+            return Regex.IsMatch(row, regex);
+        }
+        private static RowOfNumbers  obtainNumbersFromRow(string separator,string row)
+        {
+            string[] symbols = row.Split(',');
+            RowOfNumbers numbers = new RowOfNumbers();
+            numbers.Numbers = new int[symbols.Length];
+            for (int i = 0; i < symbols.Length; i++)
+            {
+                numbers.Numbers[i] = Int32.Parse(symbols[i]);
+            }
+            return numbers;
+        }
         public static string GetResultsInText(List<RowOfNumbers> rowsOfNumbers)
         {
             int lastElement = rowsOfNumbers.Count - 1;
             string result = "   [";
-            for (int i=0; i< rowsOfNumbers.Count; i++)
+            for (int i = 0; i < rowsOfNumbers.Count; i++)
             {
                 result += rowsOfNumbers[i].ToString();
                 if (i != lastElement)
@@ -50,6 +91,8 @@ namespace FibonacciServer
             }
             result += "]";
             return result;
-        } 
+        }
+
+        
     }
 }
--- FibonacciServerTests/TestTextTransfomator.cs
+++ FibonacciServerTests/TestTextTransfomator.cs
@@ -17,7 +17,7 @@ namespace FibonacciServer
             //GIVEN
             int[] expectedFirst=new int[]{ 0,1,1,2,3,5};
             int[] expectedSecond = new int[] { 5,6,7 };
-            string text = "0,1,1,2,3,5 hello1,s2,3 wow,3 5,6,7";
+            string text = "0,1,1,2,3,5?hello1,s2,3?wow,3 5,6,7?8 13 21 34";
             //WHEN
             RowOfNumbers[] result=TextMaster.TakeNumbersFromText(text);
             //THEN
